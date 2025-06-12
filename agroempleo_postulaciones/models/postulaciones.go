f89daae21e2ddcb9706e38e18b1685c26a093fea
Aprendiz Tarde <Aprendiz Tarde@YPLAPRSSVFSW013.FormacionSena.RED>
2025-06-12 15:52:39 -0500

    adelanto proyecto

--- agroempleo_postulaciones/models/postulaciones.go
+++ agroempleo_postulaciones/models/postulaciones.go
@@ -53,7 +53,7 @@ func GetPostulacionesById(id int) (v *Postulaciones, err error) {
 func GetAllPostulaciones(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(Postulaciones))
+	qs := o.QueryTable(new(Postulaciones)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
